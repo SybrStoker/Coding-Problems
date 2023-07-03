d041adf8b2b94d88faae1a62b1a25701956c1a3e
SybrStoker <gorindan2@gmail.com>
2023-07-04 00:14:35 +0700

    [#7] This is the first part of the kotlin tour.(not final)

---
+++ Messing-with-kotlin/FirstChapter/src/main/kotlin/Main.kt
@@ -0,0 +1,9 @@
+fun main() {
+    val name = "Gary"
+    val master = "Sponge Bob"
+    val bashVersion = 2
+
+    println("My $name is an adult now. He lives all by himself without $master.")
+    println("I have a déjà vu like i've seen it all before... Bash $bashVersion?")
+
+}
